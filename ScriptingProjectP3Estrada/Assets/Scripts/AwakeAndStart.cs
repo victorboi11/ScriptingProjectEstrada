f2f993629943bfadc81f638f9319109c3b5025b9
victorboi11 <725304@student.dusd.net>
2023-12-04 10:45:45 -0800

    Made a new script
    made a new script called AwakeAndStart

---
+++ ScriptingProjectP3Estrada/Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+
+}
