a266952c80c73c27270b18f1d32dc145b5a25a0c
victorboi11 <725304@student.dusd.net>
2023-12-06 11:19:52 -0800

    i made a new script call Update and Fixed update
    yay

---
+++ ScriptingProjectP3Estrada/Assets/Scripts/UpdateAndFixedUpdate.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateAndFixedUpdate : MonoBehaviour
+{
+    void FixedUpdate ()
+    {
+        Debug.Log("FixedUpdate time :" + Time.deltaTime);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update time :" + Time.deltaTime);
+    }
+}
