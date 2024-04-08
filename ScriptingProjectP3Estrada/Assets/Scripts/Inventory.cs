82afdf097042b7e3a8a4558ee35bbdf4fb6d05d3
victorboi11 <725304@student.dusd.net>
2024-04-08 11:07:19 -0700

    typo
    oops

--- ScriptingProjectP3Estrada/Assets/Scripts/Inventory.cs
+++ ScriptingProjectP3Estrada/Assets/Scripts/Inventory.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    public class Stuff
+    {
+        public int bullets;
+        public int grenades;
+        public int rockets;
+        public float fuel;
+    }
     // Start is called before the first frame update
     void Start()
     {
